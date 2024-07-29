64f0a023ef747907942376abe2db6f379a6d809f
IrinAnnPaul <114031407+IrinAnnPaul@users.noreply.github.com>
2024-07-29 14:04:32 -0400

    Create alignPredicted_args.sh

---
+++ scripts/alignPredicted_args.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# alignPredicted_args.sh
+# Usage: bash scripts/alignPredicted_args.sh 1>results/alignPredicted_args.outfmt6 2>results/logs/alignPredicted_args.err
+
+# here, you are using many sequences; each will be run and compared to swissprot db
+# let's make sure to run with -outfmt 6.  *NOTICE we are now using blastp*
+blastp -query $1 \
+    -db $2 \
+    -outfmt "6 qseqid sacc qlen slen length nident pident evalue stitle" -evalue 1e-10 -num_threads 4
